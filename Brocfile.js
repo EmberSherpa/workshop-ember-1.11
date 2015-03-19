46e9ed1824871aa693a54f73b5013fee3308439c
Taras Mankovski <tarasm@gmail.com>
2015-03-19 03:10:48 -0400

    Added Polymer

--- Brocfile.js
+++ Brocfile.js
@@ -1,5 +1,9 @@
 /* global require, module */
 
+var vulcanize   = require('broccoli-vulcanize');
+var pickFiles   = require('broccoli-static-compiler');
+var mergeTrees  = require('broccoli-merge-trees');
+
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 var app = new EmberApp();
@@ -17,4 +21,27 @@ var app = new EmberApp();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
-module.exports = app.toTree();
+var polymerVulcanize = vulcanize('app', {
+  input: 'elements.html',
+  output: 'assets/vulcanized.html',
+  csp: true,
+  inline: true,
+  strip: false,
+  excludes: {
+    imports: ["(^data:)|(^http[s]?:)|(^\/)"],
+    scripts: ["(^data:)|(^http[s]?:)|(^\/)"],
+    styles: ["(^data:)|(^http[s]?:)|(^\/)"]
+  }
+});
+
+var polymer = pickFiles('bower_components/', {
+  srcDir: '',
+  files: [
+    'webcomponentsjs/webcomponents.js',
+    'polymer/polymer.js',
+    'polymer/polymer.html'
+  ],
+  destDir: '/assets'
+});
+
+module.exports = mergeTrees([polymerVulcanize, polymer, app.toTree()]);
