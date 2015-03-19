91a688bfbab876741310ebaa2c25e43573a37863
Taras Mankovski <tarasm@gmail.com>
2015-03-19 03:20:29 -0400

    Refactored to use pods

--- app/router.js
+++ app/router.js
@@ -6,7 +6,7 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('services');
+  this.route('inject-services');
   this.route('inline-if');
   this.route('no-bind-attr');
 });
