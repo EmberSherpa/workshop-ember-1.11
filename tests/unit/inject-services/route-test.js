d3e4d4b2ee5c513ac29419eae40f91ada2200518
Taras Mankovski <tarasm@gmail.com>
2015-03-20 19:50:34 -0400

    Added Singleton Services to description

---
+++ tests/unit/inject-services/route-test.js
@@ -0,0 +1,17 @@
+import {
+  moduleFor,
+  test
+  } from 'ember-qunit';
+
+moduleFor('route:inject-services', {
+  // Specify the other units that are required for this test.
+  needs: ['controller:inject-services', 'service:local-storage']
+});
+
+test('storage service is a singleton', function(assert) {
+  var route = this.subject();
+
+  var controller = route.controllerFor('inject-services');
+
+  assert.equal(route.get('storage'), controller.get('storage'), "both route and controller storage have same object");
+});
