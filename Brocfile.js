e0aea652522a69c2ba21af1cf025a0e57c28d037
Taras Mankovski <tarasm@gmail.com>
2015-03-21 09:23:46 -0400

    Added some styles to the page

--- Brocfile.js
+++ Brocfile.js
@@ -8,6 +8,8 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 var app = new EmberApp();
 
+app.import('vendor/styles.css');
+
 // Use `app.import` to add additional libraries to the generated
 // output files.
 //
--- app/component-helper/route.js
+++ app/component-helper/route.js
@@ -1,7 +1,9 @@
 import Ember from 'ember';
 
+// BEGIN-SNIPPET component-helper-route
 export default Ember.Route.extend({
   model() {
     return [ { name: 'red-foo' }, { name: 'blue-foo' }, { name: 'yellow-foo' } ];
   }
 });
+// END-SNIPPET
